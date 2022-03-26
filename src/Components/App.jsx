f463a88cb2a4a522428f1534a511f4abc6a5e5a7
Suriyaa <76957372+suriyaaap13@users.noreply.github.com>
2022-03-26 10:43:35 +0530

    Create a notes file to store the data and import the stored data dynamically using map function

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note";
+import Notes from "./Notes";
+import notes from "../notes";
+
+function createCard(card) {
+  return <Notes title={card.title} content={card.content} />;
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createCard)}
       <Footer />
     </div>
   );
